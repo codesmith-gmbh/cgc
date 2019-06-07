5b1de7b52c9ab2b41f22a83d7a071dd6b6f7ea29
Stanislas Nanchen <stan@hood.ch>
2019-06-07 09:53:47 +0200

    pg rds ssl connection

---
+++ cgcpg/connection.go
@@ -0,0 +1,89 @@
+package cgcpg
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/rds/rdsutils"
+	"github.com/gobuffalo/packr/v2"
+	"github.com/jackc/pgx"
+	"github.com/pkg/errors"
+	"strconv"
+)
+
+const rootCertificateFile = "rds-ca-2015-root.pem"
+
+var certPool = createCertPool()
+
+func createCertPool() *x509.CertPool {
+	certBox := packr.New("Certificates", "./certificates")
+	certPool := x509.NewCertPool()
+	pemCert, err := certBox.Find(rootCertificateFile)
+	if err != nil {
+		panic(err)
+	}
+	certPool.AppendCertsFromPEM(pemCert)
+	return certPool
+}
+
+type ConnConfigOption interface {
+	configure(config *pgx.ConnConfig) error
+}
+
+type RdsIamConfigOption struct {
+	Host, Username, Database string
+	Port                     int
+	Config                   aws.Config
+}
+
+func (r RdsIamConfigOption) configure(config *pgx.ConnConfig) error {
+	endpoint := r.Host + ":" + strconv.Itoa(r.Port)
+	token, err := rdsutils.BuildAuthToken(endpoint, r.Config.Region, r.Username, r.Config.Credentials)
+	if err != nil {
+		return errors.Wrapf(err, "could not get an auth token for the host %s, port %d and username %s", r.Host, r.Port, r.Username)
+	}
+	config.Host = r.Host
+	config.Port = uint16(r.Port)
+	config.User = r.Username
+	config.Database = r.Database
+	config.Password = token
+	return nil
+}
+
+type TlsConfigOption struct {
+	Host string
+}
+
+func (t TlsConfigOption) configure(config *pgx.ConnConfig) error {
+	tlsConfig := &tls.Config{
+		RootCAs:    certPool,
+		ServerName: t.Host,
+	}
+	config.TLSConfig = tlsConfig
+	return nil
+}
+
+func NewConfig(options ...ConnConfigOption) (pgx.ConnConfig, error) {
+	config := pgx.ConnConfig{}
+	for _, o := range options {
+		if err := o.configure(&config); err != nil {
+			return pgx.ConnConfig{}, err
+		}
+	}
+	return config, nil
+}
+
+func NewRdsSslConnection(cfg aws.Config, host string, port int, database, username string) (*pgx.Conn, error) {
+	config, err := NewConfig(
+		RdsIamConfigOption{Host: host, Port: port, Database: database, Username: username, Config: cfg},
+		TlsConfigOption{Host: host},
+	)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not create the connection configuration")
+	}
+	conn, err := pgx.Connect(config)
+	if err != nil {
+		return nil, errors.Wrapf(err, "could not connect to the database %s on host %s with user %s", database, host, username)
+	}
+	return conn, nil
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,14 @@ go 1.12
 
 require (
 	github.com/aws/aws-sdk-go-v2 v0.9.0
-	github.com/pkg/errors v0.8.0
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gobuffalo/packr/v2 v2.3.2
+	github.com/jackc/fake v0.0.0-20150926172116-812a484cc733 // indirect
+	github.com/jackc/pgx v3.4.0+incompatible
+	github.com/lib/pq v1.1.1 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/satori/go.uuid v1.2.0 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
